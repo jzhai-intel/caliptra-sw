94bfe0585e7035e05c3b6368feb0a68df9d99bb5
Arthur Heymans <arthur.heymans@9elements.com>
2025-05-15 18:09:38 +0200

    Add MLDSA RT_ALIAS CMD implementation
    Signed-off-by: Arthur Heymans <arthur.heymans@9elements.com>

--- api/src/mailbox.rs
+++ api/src/mailbox.rs
@@ -453,6 +453,7 @@ pub enum MailboxReq {
     GetTaggedTci(GetTaggedTciReq),
     GetFmcAliasEcc384Cert(GetFmcAliasEcc384CertReq),
     GetRtAliasEcc384Cert(GetRtAliasEcc384CertReq),
+    GetRtAliasMlDsa87Cert(GetRtAliasMlDsa87CertReq),
     IncrementPcrResetCounter(IncrementPcrResetCounterReq),
     QuotePcrs(QuotePcrsReq),
     ExtendPcr(ExtendPcrReq),
@@ -515,6 +516,7 @@ impl MailboxReq {
             MailboxReq::GetTaggedTci(req) => Ok(req.as_bytes()),
             MailboxReq::GetFmcAliasEcc384Cert(req) => Ok(req.as_bytes()),
             MailboxReq::GetRtAliasEcc384Cert(req) => Ok(req.as_bytes()),
+            MailboxReq::GetRtAliasMlDsa87Cert(req) => Ok(req.as_bytes()),
             MailboxReq::IncrementPcrResetCounter(req) => Ok(req.as_bytes()),
             MailboxReq::QuotePcrs(req) => Ok(req.as_bytes()),
             MailboxReq::ExtendPcr(req) => Ok(req.as_bytes()),
@@ -575,6 +577,7 @@ impl MailboxReq {
             MailboxReq::GetTaggedTci(req) => Ok(req.as_mut_bytes()),
             MailboxReq::GetFmcAliasEcc384Cert(req) => Ok(req.as_mut_bytes()),
             MailboxReq::GetRtAliasEcc384Cert(req) => Ok(req.as_mut_bytes()),
+            MailboxReq::GetRtAliasMlDsa87Cert(req) => Ok(req.as_mut_bytes()),
             MailboxReq::IncrementPcrResetCounter(req) => Ok(req.as_mut_bytes()),
             MailboxReq::QuotePcrs(req) => Ok(req.as_mut_bytes()),
             MailboxReq::ExtendPcr(req) => Ok(req.as_mut_bytes()),
@@ -635,6 +638,7 @@ impl MailboxReq {
             MailboxReq::GetTaggedTci(_) => CommandId::DPE_GET_TAGGED_TCI,
             MailboxReq::GetFmcAliasEcc384Cert(_) => CommandId::GET_FMC_ALIAS_ECC384_CERT,
             MailboxReq::GetRtAliasEcc384Cert(_) => CommandId::GET_RT_ALIAS_ECC384_CERT,
+            MailboxReq::GetRtAliasMlDsa87Cert(_) => CommandId::GET_RT_ALIAS_MLDSA87_CERT,
             MailboxReq::IncrementPcrResetCounter(_) => CommandId::INCREMENT_PCR_RESET_COUNTER,
             MailboxReq::QuotePcrs(_) => CommandId::QUOTE_PCRS,
             MailboxReq::ExtendPcr(_) => CommandId::EXTEND_PCR,
@@ -895,6 +899,17 @@ pub struct GetRtAliasEcc384CertReq {
 }
 impl Request for GetRtAliasEcc384CertReq {
     const ID: CommandId = CommandId::GET_RT_ALIAS_ECC384_CERT;
+    type Resp = GetRtAliasCertResp;
+}
+
+// GET_RT_ALIAS_MLDSA87_CERT
+#[repr(C)]
+#[derive(Default, Debug, IntoBytes, FromBytes, Immutable, KnownLayout, PartialEq, Eq)]
+pub struct GetRtAliasMlDsa87CertReq {
+    pub header: MailboxReqHeader,
+}
+impl Request for GetRtAliasMlDsa87CertReq {
+    const ID: CommandId = CommandId::GET_RT_ALIAS_MLDSA87_CERT;
     type Resp = GetRtAliasCertResp;
 }
 
